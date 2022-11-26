667b7ef3bb99ebc2f919d0841ba1beab83dce32a
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-11-26 20:32:57 +0200

    Update RestrictedRoute.jsx

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -3,6 +3,5 @@ import { Navigate } from 'react-router-dom';
 
 export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn } = useAuth();
-  console.log('isloggedin:', isLoggedIn);
   return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
 };
